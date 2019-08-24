c5f18071236146f8f65b14105a13132a0327d225
hschow <47091224+hschow@users.noreply.github.com>
2019-08-24 16:37:58 -0700

    Update Practice01MathRecursive.java

--- Practice01MathRecursive.java
+++ Practice01MathRecursive.java
@@ -1,1 +1,30 @@
+public class Practice01MathRecursive implements Practice01Math{
+    public int fact (int n) {
+        if (n==0 || n==1){
+            return 1;
+        }
+        else{
+            if (n<0){
+                return 0;
+            }
+            else {
+                return n * fact(n - 1); //n is negative goes forever
+            }
+        }
+    }
 
+    public int fib (int n){
+        if (n==0 || n==1){
+            return n;
+        }
+        else{
+            if (n<0){
+                return 0;
+            }
+            else {
+                return fib(n - 1) + fib(n - 2);
+            }
+        }
+    }
+
+}
