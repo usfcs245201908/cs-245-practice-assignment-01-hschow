fc0cec8e1b2ee5183d2e386b0d4f6d6da37f7cfe
hschow <47091224+hschow@users.noreply.github.com>
2019-08-29 23:09:49 -0700

    Update Practice01MathIterative.java

--- Practice01MathIterative.java
+++ Practice01MathIterative.java
@@ -4,15 +4,20 @@ public class Practice01MathIterative implements Practice01Math{
         if (n == 0 || n == 1) {
             return 1;
         } else {
-            int total = 1;
-            if (n<1){
-                return 1;
+            if (n<0){
+                throw new IllegalArgumentException();
             }
-            while (n > 1) {
-                total = total * n;
-                n = n - 1;
+            else{
+                int total = 1;
+                if (n<1){
+                    return 1;
+                }
+                while (n > 1) {
+                    total = total * n;
+                    n = n - 1;
+                }
+                return total;
             }
-            return total;
         }
     }
 
@@ -21,19 +26,24 @@ public class Practice01MathIterative implements Practice01Math{
         if (n == 0 || n == 1) {
             return n;
         } else {
-            int x = 1; //this is n = 1, or n-1
-            int y = 0; //this is n = 0, or n-2
-            int z = 0; // this is for the running total
-            if (n<1){
-                return 1;
+            if (n<0){
+                throw new IllegalArgumentException();
             }
-            while (n > 1) {
-                z = x + y; // this adds the two previous numbers
-                y = x; //this changes n-1 to n-2
-                x = z;  // this creates a new n-1
-                n = n - 1;
+            else {
+                int x = 1; //this is n = 1, or n-1
+                int y = 0; //this is n = 0, or n-2
+                int z = 0; // this is for the running total
+                if (n<1){
+                    return 1;
+                }
+                while (n > 1) {
+                    z = x + y; // this adds the two previous numbers
+                    y = x; //this changes n-1 to n-2
+                    x = z;  // this creates a new n-1
+                    n = n - 1;
+                }
+                return z;
             }
-            return z;
         }
     }
 }
